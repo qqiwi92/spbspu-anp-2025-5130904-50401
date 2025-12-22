e2207801a606cde1c04f76b16219ae67b3a30b42
qiwi <qiwi@qiwi.qiwi>
2025-12-22 19:09:08 +0300

    add: diamond

--- levkin.dima/P5/main.cpp
+++ levkin.dima/P5/main.cpp
@@ -41,5 +41,26 @@ class Rectangle : public Shape {
   }
 };
 
+class Diamond : public Shape {
+  rectangle_t data_fields;
+
+  Diamond(point_t center, double width, double height)
+      : data_fields{width, height, center} {}
+  double getArea() const override {
+    return .5 * data_fields.height * data_fields.width;
+  }
+  virtual rectangle_t getFrameRect() const override { return data_fields; }
+  virtual void move(point_t dest) override {
+    data_fields.pos = {dest.x, dest.y};
+  }
+  virtual void move(double dx, double dy) override {
+    move({data_fields.pos.x + dx, data_fields.pos.y + dy});
+  }
+  virtual void scale(double k) override {
+    data_fields.width *= k;
+    data_fields.height *= k;
+  }
+};
+
 } // namespace levkin
 int main() {}
