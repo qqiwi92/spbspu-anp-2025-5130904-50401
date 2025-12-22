eee4e551f766ff190bccfcdb2d344eb44fbba8fb
qiwi <qiwi@qiwi.qiwi>
2025-12-22 19:03:18 +0300

    add: overriding methods in rect

--- levkin.dima/P5/main.cpp
+++ levkin.dima/P5/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+
+namespace levkin {
+
 struct point_t {
   double x, y;
 };
-struct rectangle_t { 
-    double width, height;
-    point_t pos; 
+struct rectangle_t {
+  double width, height;
+  point_t pos;
 };
-class Shape
-{
+class Shape {
 public:
   virtual ~Shape() = default;
 
@@ -18,7 +20,26 @@ public:
   virtual void scale(double k) = 0;
 };
 
-int main ()
-{
-}
+class Rectangle : public Shape {
+  rectangle_t data_fields;
 
+  Rectangle(point_t center, double width, double height)
+      : data_fields{width, height, center} {}
+  double getArea() const override {
+    return data_fields.height * data_fields.width;
+  }
+  virtual rectangle_t getFrameRect() const override { return data_fields; }
+  virtual void move(point_t dest) override {
+    data_fields.pos = {dest.x, dest.y};
+  }
+  virtual void move(double dx, double dy) override {
+    move({data_fields.pos.x + dx, data_fields.pos.y + dy});
+  }
+  virtual void scale(double k) override {
+    data_fields.width *= k;
+    data_fields.height *= k;
+  }
+};
+
+} // namespace levkin
+int main() {}
